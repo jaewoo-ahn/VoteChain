0fca98b9f17655a93624a29f314bc2fdb9696ee8
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2023-06-22 15:48:07 +0900

    vote intro

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <div className="pt-8">
       <div
-        className="flex flex-col justify-center items-start h-screen bg-cover p-8"
+        className="flex flex-col justify-center items-start h-screen bg-cover p-12"
         style={{backgroundImage: "url('/assets/images/background.png')"}}
       >
         <h1 className="text-white text-7xl">도달 범위 확장</h1>
@@ -24,6 +24,35 @@ const Main = () => {
           </button>
         </div>
       </div>
+      <div className="flex h-[80%]">
+        <img
+          className="w-[68%] rounded-b-full"
+          src="/assets/images/jelly_with_sea.png"
+          alt="sea with jelly fish"
+        />
+        <div className=" flex flex-col items-start justify-start p-8">
+          <h1 className="text-4xl font-bold py-4">
+            당사의 제품은 콘텐츠 배포의 장벽을 허물고 있습니다
+          </h1>
+          <p className="py-4 text-xl">
+            Pinata에서는 브랜드와 크리에이터가 상상력을 마음껏 발휘할 수 있도록
+            지원합니다.
+          </p>
+          <p className="py-4 text-xl">
+            당사의 제품은 모든 유형의 미디어를 규모에 따라 안전하게 배포할 수
+            있도록 지원하여 크리에이티브 에이전시를 제한하는 제한적인 플랫폼,
+            중개자 및 알고리즘으로부터 자유롭게 해줍니다.
+          </p>
+          <p className="py-4 text-xl">
+            당신이 그것을 만들 수 있다면, 우리는 당신이 그것을 공유하도록 도울
+            수 있습니다. 너의 길.
+          </p>
+          <button className="py-2 px-4 bg-[#3e8ae6] text-white border-white rounded-full">
+            구축시작{" "}
+          </button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
